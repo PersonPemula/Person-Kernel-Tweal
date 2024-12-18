e3ed6e5ccc2b73472e60d0ef57656677564f1c32
Person <161296979+PersonPemula@users.noreply.github.com>
2024-12-19 01:10:35 +0800

    Add files via upload

---
+++ customize.sh
@@ -0,0 +1,43 @@
+#!/system/bin/sh
+ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+ui_print "| ⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀"
+ui_print "| ⢀⢀⢀⢀⢀⢀⢀⡤⠒⠋⠉⠉⠉⠉⠉⠑⠲⢄⢀⢀⢀⢀⢀⢀⢀"
+ui_print "| ⢀⢀⢀⢀⢀⠔⢁⡠⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠉⢢⢀⢀⢀⢀⢀"
+ui_print "| ⢀⢀⢀⢠⠃⣴⡇⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠱⡄⢀⢀⢀"
+ui_print "| ⢀⢀⢠⠃⢀⣿⣧⣀⢀⢀⣀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⠹⡀⢀⢀"
+ui_print "| ⢀⢀⡇⢀⢀⠹⣶⣿⣿⣿⣿⣿⣶⣶⣶⣤⡀⢀⢀⣴⡿⢀⡇⢀⢀"
+ui_print "| ⢀⢀⡇⢀⢀⢀⠤⣾⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⢀⡇⢀⢀"
+ui_print "| ⢀⢀⡇⢀⣠⣤⣶⣿⣿⣧⣉⣿⣿⡟⣃⣿⢿⡿⢀⢀⢀⢀⡇⢀⢀"
+ui_print "| ⢀⢀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢀⢀⢀⢀⢀⡰⠁⢀⢀"
+ui_print "| ⢀⢀⢀⠘⣿⣿⣿⣿⣿⣏⡀⢀⣸⣿⣿⢀⢀⢀⢀⢀⡰⠃⢀⢀⢀"
+ui_print "| ⢀⢀⢀⢀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⡏⢀⢀⢀⣠⠞⠁⢀⢀⢀⢀"
+ui_print "| ⢀⢀⢀⢀⢀⢀⠈⠙⠿⢿⣿⣿⣿⣿⣇⡠⠔⠊⠁⢀⢀⢀⢀⢀⢀"
+ui_print "| ⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀⢀"
+ui_print " "
+sleep 1
+ui_print " Module info: "
+ui_print " • Name            : Person-Kernel-Tweak"
+ui_print " • Codename        : person-kt"
+ui_print " • Status          : Release On Public"
+ui_print " • Owner           : Person "
+ui_print " • Build Date      : 19-12-2024"
+ui_print " • Support Varians : Universal "
+ui_print " "
+ui_print " Device Info: "
+ui_print " • Device : $(getprop ro.product.manufacturer), $(getprop ro.product.device)"
+sleep 1
+ui_print " • SELinux Status : $(getenforce)"
+sleep 1
+ui_print " • Kernel Version : $(uname -r)"
+sleep 1
+ui_print " "
+sleep 1
+ui_print " Thanks To:"
+ui_print " — Person//Mod"
+ui_print " — Zyarexx//Own"
+ui_print " "
+sleep 1
+ui_print " Please Reboot Your Device Now ‼️ "
+ui_print " "
+sleep 1
+ui_print "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
---
+++ service.sh
@@ -0,0 +1,1 @@
+chmod 0777 /data/adb/modules/person-kt/kerneltweak && exec /data/adb/modules/person-kt/kerneltweak
